b80a7fab29609739021f9d895646f0492effc016
Chloe Zhou <chloez0607@gmail.com>
2024-09-08 13:09:58 +0800

    refactor fetch breed list api call.

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,35 +1,9 @@
-import { useState, useEffect } from "react";
-
-var localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
 function useBreedList(animal) {
-  var [breedList, setBreedList] = useState([]);
-  var [status, setStatus] = useState("unloaded");
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-
-      var res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      var json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  const results = useQuery(["breeds", animal], fetchBreedList);
+  return [results?.data?.breeds ?? [], results.status];
 }
 
 export default useBreedList;
